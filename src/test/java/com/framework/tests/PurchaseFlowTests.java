960b10ed6065f0ad7d2e9e1c5f8ca1fbd328283a
German Uriarte <uriarte.german@gmail.com>
2024-11-06 14:44:32 -0500

    maven, selenium setup, first test

---
+++ src/test/java/com/framework/tests/PurchaseFlowTests.java
@@ -0,0 +1,39 @@
+package com.framework.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class PurchaseFlowTests {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testPurchaseFlowEndToEnd() {
+
+    }
+
+
+
+}
