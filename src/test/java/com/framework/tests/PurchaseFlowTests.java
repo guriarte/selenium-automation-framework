1438fda1922193aa5046badcc5f4dbea4f0389e8
German Uriarte <uriarte.german@gmail.com>
2024-11-11 12:55:00 -0500

    completed testPurchaseFlowEndToEnd

--- src/test/java/com/framework/tests/PurchaseFlowTests.java
+++ src/test/java/com/framework/tests/PurchaseFlowTests.java
@@ -2,20 +2,16 @@ package com.framework.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import junit.framework.Assert;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
+
 
 public class PurchaseFlowTests {
     private WebDriver driver;
@@ -29,6 +25,7 @@ public class PurchaseFlowTests {
     void setup() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        driver.manage().window().maximize();
     }
 
     @AfterEach
@@ -52,7 +49,7 @@ public class PurchaseFlowTests {
         submitButton.click();
 
         // add network call for login?
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.urlContains("/account"));
         Assert.assertTrue("The URL does not contain /account", driver.getCurrentUrl().contains("/account"));
 
@@ -84,6 +81,71 @@ public class PurchaseFlowTests {
 
         WebElement addedToCartPopup = driver.findElement(By.cssSelector("[id=\"toast-container\"]"));
         Assert.assertTrue("Added to Cart Popup is not visible", addedToCartPopup.isDisplayed());
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("[id=\"toast-container\"]")));
 
+        WebElement shoppingCartButton = driver.findElement(By.cssSelector("[data-test=\"nav-cart\"]"));
+        shoppingCartButton.click();
+
+        wait.until(ExpectedConditions.urlContains("/checkout"));
+        WebElement rowContainingProduct = driver.findElement(By.xpath("//tr/td/span[contains(text(), \"Combination Pliers\")]"));
+        WebElement quantityInputBoxForProduct = rowContainingProduct.findElement(By.xpath("//input"));
+        quantityInputBoxForProduct.clear();
+        quantityInputBoxForProduct.sendKeys("1");
+
+        WebElement firstProceedToCheckoutButton = driver.findElement(By.cssSelector("[data-test=\"proceed-1\"]"));
+        firstProceedToCheckoutButton.click();
+
+        WebElement secondProceedToCheckoutButton = driver.findElement(By.cssSelector("[data-test=\"proceed-2\"]"));
+        secondProceedToCheckoutButton.click();
+
+        WebElement addressTextField = driver.findElement(By.cssSelector("[data-test=\"address\"]"));
+        addressTextField.clear();
+        addressTextField.sendKeys("Fake Address");
+
+        WebElement cityTextField = driver.findElement(By.cssSelector("[data-test=\"city\"]"));
+        cityTextField.clear();
+        cityTextField.sendKeys("Fake City");
+
+        WebElement stateTextField = driver.findElement(By.cssSelector("[data-test=\"state\"]"));
+        stateTextField.clear();
+        stateTextField.sendKeys("Fake State");
+
+        WebElement countryTextField = driver.findElement(By.cssSelector("[data-test=\"country\"]"));
+        countryTextField.clear();
+        countryTextField.sendKeys("Fake Country");
+
+        WebElement postCodeTextField = driver.findElement(By.cssSelector("[data-test=\"postcode\"]"));
+        postCodeTextField.clear();
+        postCodeTextField.sendKeys("12345");
+
+        WebElement thirdProceedToCheckoutButton = driver.findElement(By.cssSelector("[data-test=\"proceed-3\"]"));
+        thirdProceedToCheckoutButton.click();
+
+        WebElement paymentDropdownElement = driver.findElement(By.cssSelector("[data-test=\"payment-method\"]"));
+        Select paymentDropdown = new Select(paymentDropdownElement);
+
+        paymentDropdown.selectByValue("credit-card");
+
+        WebElement creditCardNumberTextBox = driver.findElement(By.cssSelector("[data-test=\"credit_card_number\"]"));
+        creditCardNumberTextBox.clear();
+        creditCardNumberTextBox.sendKeys("1234-5678-9012-3456");
+
+        WebElement expirationDateTextBox = driver.findElement(By.cssSelector("[data-test=\"expiration_date\"]"));
+        expirationDateTextBox.clear();
+        expirationDateTextBox.sendKeys("11/2032");
+
+        WebElement cvvTextBox = driver.findElement(By.cssSelector("[data-test=\"cvv\"]"));
+        cvvTextBox.clear();
+        cvvTextBox.sendKeys("123");
+
+        WebElement cardHolderNameTextBox = driver.findElement(By.cssSelector("[data-test=\"card_holder_name\"]"));
+        cardHolderNameTextBox.clear();
+        cardHolderNameTextBox.sendKeys("Jane Doe");
+
+        WebElement confirmButton = driver.findElement(By.cssSelector("[data-test=\"finish\"]"));
+        confirmButton.click();
+        confirmButton.click();
     }
+
 }
+
